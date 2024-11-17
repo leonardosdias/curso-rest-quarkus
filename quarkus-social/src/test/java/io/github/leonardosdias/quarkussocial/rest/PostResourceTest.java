27d24d7fdd349f1b1655695b3a5693517a3c5d33
Leonardo Dias <leonnardosouza96@gmail.com>
2024-11-17 14:00:01 -0300

    Testes unitários PostResource pt 2

--- quarkus-social/src/test/java/io/github/leonardosdias/quarkussocial/rest/PostResourceTest.java
+++ quarkus-social/src/test/java/io/github/leonardosdias/quarkussocial/rest/PostResourceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import javax.inject.Inject;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,17 +23,47 @@ import io.github.leonardosdias.quarkussocial.rest.dto.*;
 public class PostResourceTest {
     @jakarta.inject.Inject
     UserRepository userRepository;
+
+    @jakarta.inject.Inject
+    FollowerRepository followerRepository;
+
+    @jakarta.inject.Inject
+    PostRepository postRepository;
+
     Long userId;
+    Long userNotFollowerId;
+    Long userFollowerId;
 
     @BeforeEach
     @Transactional
     public void setUP() {
         var user = new User();
         user.setAge(30);
-        user.setName("Usuário Teste");
+        user.setName("Usuário Teste padrão");
         userRepository.persist(user);
         userId = user.getId();
 
+        var userNotFollower = new User();
+        userNotFollower.setAge(40);
+        userNotFollower.setName("Usuário Teste não seguidor");
+        userRepository.persist(userNotFollower);
+        userNotFollowerId = userNotFollower.getId();
+
+        var userFollower = new User();
+        userFollower.setAge(40);
+        userFollower.setName("Usuário Teste seguidor");
+        userRepository.persist(userFollower);
+        userFollowerId = userFollower.getId();
+
+        var follower = new Follower();
+        follower.setUser(user);
+        follower.setFollower(userFollower);
+        followerRepository.persist(follower);
+
+        var post = new Post();
+        post.setUser(user);
+        post.setText("Teste");
+        postRepository.persist(post);
     }
 
     @Test
@@ -72,4 +103,68 @@ public class PostResourceTest {
                 .statusCode(404);
     }
 
+    @Test
+    @DisplayName("deve retornar 404 quando o usuário não existir")
+    public void listPostUserNotFoundTest() {
+        var inexistentUser = 22222;
+        given()
+                .pathParam("userId", inexistentUser)
+                .when()
+                .get()
+                .then()
+                .statusCode(404);
+
+    }
+
+    @Test
+    @DisplayName("deve retornar 404 quando não enviar o id do seguidor pelo header pela requisição")
+    public void listPostFollowerHeaderNotSendTest() {
+        given()
+                .pathParam("userId", userId)
+                .when()
+                .get()
+                .then()
+                .statusCode(500);
+    }
+
+    @Test
+    @DisplayName("deve retornar 404 quando o seguidor não existir")
+    public void listPostFollowerNotFoundTest() {
+
+        var inexistentFollower = 22222;
+        given()
+                .pathParam("userId", userId)
+                .header("followerId", inexistentFollower)
+                .when()
+                .get()
+                .then()
+                .statusCode(400);
+
+    }
+
+    @Test
+    @DisplayName("deve retornar 403 quando o usuário não for um seguidor")
+    public void listPostNotAFollowerTest() {
+
+        given()
+                .pathParam("userId", userId)
+                .header("followerId", userNotFollowerId)
+                .when()
+                .get()
+                .then()
+                .statusCode(403);
+
+    }
+
+    @Test
+    @DisplayName("deve retornar os posts de um usuário")
+    public void listPostsTest() {
+        given()
+        .pathParam("userId", userId)
+        .header("followerId", userFollowerId)
+        .when()
+        .get()
+        .then()
+        .statusCode(200);
+    }
 }
